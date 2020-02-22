289e7b3112ded6af04fdf18dee6b96580b520f27
BobNarwhalKing <danceflurry4366@gmail.com>
2020-02-21 18:13:14 -0600

    Classes and functions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            WriteLineHello();
+            Apple GrannySmith = new Apple("Granny Smith", "Green");
+            Apple Gala = new Apple("Gala");
+            GrannySmith.info();
+            Gala.info();
+        }
+        public static void WriteLineHello()
+        {
             Console.WriteLine("Hello World!");
+        } 
+        class Apple
+        {
+            private string color;
+            private string type;
+
+            //Constructor Method
+            public Apple(string t, string c)
+            {
+                type = t;
+                color = c;
+            }
+            //Second Contructor Method Called Overloading Constructors
+            public Apple(string t)
+            {
+                type = t;
+                color = "Red";
+            }
+            public void info()
+            {
+                Console.WriteLine($"Type: {type}");
+                Console.WriteLine($"Color: {color}");
+            }
         }
     }
 }
